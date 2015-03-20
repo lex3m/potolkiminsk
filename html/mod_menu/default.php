2e0e7b81b9c3f4a7c39f7ff3e02071a9e7677951
Alexey Fedorenko <lex24@ukr.net>
2015-03-20 14:57:57 +0200

    changes mod_menu

--- html/mod_menu/default.php
+++ html/mod_menu/default.php
@@ -12,7 +12,7 @@ defined('_JEXEC') or die;
 // Note. It is important to remove spaces between elements.
 ?>
 <?php // The menu class is deprecated. Use nav instead. ?>
-
+// Test this shit!!
 	<ul class="<?php echo $class_sfx;?>"<?php
 		$tag = '';
 
